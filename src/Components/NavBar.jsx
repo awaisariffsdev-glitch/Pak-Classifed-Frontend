b73aaa1fefa0a1bfcf60b12b6780cb3914104c6e
awaisariffsdev-glitch <285652427+awaisariffsdev-glitch@users.noreply.github.com>
2026-07-03 14:46:44 +0500

    Changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,34 +1,37 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react'
+import { Link } from 'react-router-dom'
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import SignUp from './SignUp';
+import LogIn from './LogIn';
+// import AddCar from './AddCar';
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-secondary" style={{position:"sticky",top:"0%",zIndex:"1000"}}>
-      <Container>
-        <Navbar.Brand href="#home">PakClassifed</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="m-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Categories" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    <nav className='d-flex align-items-center position-sticky top-0 z-3  text-light justify-content-between' style={{ backgroundColor: "#1D2023" }}>
+      <div className="" style={{ width: "16%" }}><Link><img src="img/image3.png" className='w-100' alt="" /></Link></div>
+      <div className="d-flex gap-4 align-items-center">
+        <Link className='text-decoration-none text-light'>Home</Link>
+        <Link className='text-decoration-none text-light'>About</Link>
+        <NavDropdown title="Categories" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            Another action
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">
+            Separated link
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Link className='text-decoration-none text-light'>Contact</Link>
+      </div>
+      <div className=" d-flex gap-2 justify-content-center" style={{ width: "20%" }}>
+        <SignUp />
+        <LogIn />
+        {/* <AddCar/> */}
+      </div>
+    </nav>
+  )
 }
 
-export default NavBar;
+export default NavBar
